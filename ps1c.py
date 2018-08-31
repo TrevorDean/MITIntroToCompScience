90d36da99cd97fe05fdd0dc85bb70368fa7ac300
Trevor <TrevorDean@users.noreply.github.com>
2018-08-30 23:25:20 -0500

    Fixed the addition without using brackets error

--- ps1c.py
+++ ps1c.py
@@ -3,10 +3,8 @@
 annual_salary = float(input("Enter your annual salary: "))
 portion_saved = 10000 # Percent of salary to save each month expressed as integer but reresenting
                       # ten thousandths of a percent
-
 total_cost = 1000000
 semi_annual_raise = .07
-
 portion_down_payment = total_cost * 0.25
 current_savings = 0
 r = 0.04
@@ -15,7 +13,7 @@ monthly_salary = annual_salary / 12
 high = portion_saved
 low = 0
 after36_savings = 0
-guess_portion_saved = high+low/2
+guess_percent = (high + low) / 2
 steps = 0
 
 def amount_saved(months, current_savings, portion_saved, monthly_salary):
@@ -29,19 +27,20 @@ def amount_saved(months, current_savings, portion_saved, monthly_salary):
 
 while after36_savings > 250100 or after36_savings < 249900:
     steps += 1
-    portion_saved = .0001 * guess_portion_saved
+    portion_saved = .0001 * guess_percent
     after36_savings = amount_saved(months, current_savings, portion_saved, monthly_salary)
     if 250100 >= after36_savings >= 249900:
-        print("Best savings rate:", round(guess_portion_saved*.0001, 4))
+        print("Best savings rate:", round(guess_percent * .0001, 4))
         print("Steps in bisection search:", steps)
     if after36_savings > 250100:
-        high = guess_portion_saved/2
+        high = guess_percent
     else:
-        low = guess_portion_saved
+        low = guess_percent
 
     if low == 10000:
         print("It is not possible to pay the down payment in three years.")
         print("steps:", steps)
         break
-    guess_portion_saved = high + low / 2
+    guess_percent = (high + low) / 2
+
 
