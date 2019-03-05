2335da493584985a0035ebb2effbbd4d8ebe724c
Trevor <TrevorDean@users.noreply.github.com>
2019-03-04 19:58:46 -0600

    Finished part 1 of ps2

--- ps2/hangman.py
+++ ps2/hangman.py
@@ -54,15 +54,15 @@ def is_word_guessed(secret_word, letters_guessed):
     '''
     secret_word: string, the word the user is guessing; assumes all letters are
       lowercase
-      letters_guessed: list (of letters), which letters have been guessed so far;
-      assumes that all letters are lowercase
-    returns: boolean, True if all the letters of secret_word are in letters_guessed;
-      False otherwise
+      letters_guessed: list (of letters), which letters have been guessed so
+      far. Assumes that all letters are lowercase.
+    returns: boolean, True if all the letters of secret_word are in
+    letters_guessed; False otherwise
     '''
     secret_word_split = list(secret_word)
     for letter in secret_word_split:
         if letter in letters_guessed:
-            letters_guessed.remove(letter)
+            pass
         else:
             return False
     return True
@@ -72,23 +72,29 @@ def get_guessed_word(secret_word, letters_guessed):
     '''
     secret_word: string, the word the user is guessing
     letters_guessed: list (of letters), which letters have been guessed so far
-    returns: string, comprised of letters, underscores (_), and spaces that represents
-      which letters in secret_word have been guessed so far.
+    returns: string, comprised of letters, underscores (_), and spaces that
+    represents which letters in secret_word have been guessed so far.
     '''
-    # FILL IN YOUR CODE HERE AND DELETE "pass"
-    pass
-
+    secret_word_split = list(secret_word)
+    secret_word_decoded = ''
+    for letter in secret_word_split:
+        if letter in letters_guessed:
+            secret_word_decoded+=letter
+        else:
+            secret_word_decoded+='_ '
+    return secret_word_decoded
 
 
 def get_available_letters(letters_guessed):
     '''
     letters_guessed: list (of letters), which letters have been guessed so far
-    returns: string (of letters), comprised of letters that represents which letters have not
-      yet been guessed.
+    returns: string (of letters), comprised of letters that represents which
+    letters have not yet been guessed.
     '''
-    # FILL IN YOUR CODE HERE AND DELETE "pass"
-    pass
-
+    letters_available = list(string.ascii_lowercase)
+    for letter in letters_guessed:
+        letters_available.remove(letter)
+    return letters_available
 
 
 def hangman(secret_word):
@@ -136,8 +142,8 @@ def match_with_gaps(my_word, other_word):
     my_word: string with _ characters, current guess of secret word
     other_word: string, regular English word
     returns: boolean, True if all the actual letters of my_word match the
-        corresponding letters of other_word, or the letter is the special symbol
-        _ , and my_word and other_word are of the same length;
+        corresponding letters of other_word, or the letter is the special
+        symbol _ , and my_word and other_word are of the same length;
         False otherwise:
     '''
     # FILL IN YOUR CODE HERE AND DELETE "pass"
@@ -148,10 +154,10 @@ def match_with_gaps(my_word, other_word):
 def show_possible_matches(my_word):
     '''
     my_word: string with _ characters, current guess of secret word
-    returns: nothing, but should print out every word in wordlist that matches my_word
-             Keep in mind that in hangman when a letter is guessed, all the positions
-             at which that letter occurs in the secret word are revealed.
-             Therefore, the hidden letter(_ ) cannot be one of the letters in the word
+    returns: nothing, but should print out every word in wordlist that matches
+    my_word Keep in mind that in hangman when a letter is guessed, all the
+    positions at which that letter occurs in the secret word are revealed.
+    Therefore, the hidden letter(_ ) cannot be one of the letters in the word
              that has already been revealed.
 
     '''
@@ -174,7 +180,8 @@ def hangman_with_hints(secret_word):
     * Before each round, you should display to the user how many guesses
       s/he has left and the letters that the user has not yet guessed.
 
-    * Ask the user to supply one guess per round. Make sure to check that the user guesses a letter
+    * Ask the user to supply one guess per round. Make sure to check that the
+      user guesses a letter
 
     * The user should receive feedback immediately after each guess
       about whether their guess appears in the computer's word.
--- ps2/test.py
+++ ps2/test.py
@@ -1,11 +1,9 @@
-secret_word = 'apple'
-letters_guessed = ['a', 'p', 'p', 'p', 'l']
-def is_word_guessed(secret_word, letters_guessed):
-    secret_word_split = list(secret_word)
-    for letter in secret_word_split:
-        if letter in letters_guessed:
-            letters_guessed.remove(letter)
-        else:
-            return False
-    return True
-print (is_word_guessed(secret_word, letters_guessed))
+import string
+def get_available_letters(letters_guessed):
+    letters_available = list(string.ascii_lowercase)
+    for letter in letters_guessed:
+        letters_available.remove(letter)
+    return letters_available
+
+letters_guessed = ['e', 'i', 'k', 'p', 'r', 's']
+print (get_available_letters(letters_guessed))
